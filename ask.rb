7e25e1d4530ed521178a12d0dc99478a4a798a6e
Damoon Rashidi <damoon.rashidi@gmail.com>
2024-02-01 15:41:41 +0100

    V1.3.0

--- ask.rb
+++ ask.rb
@@ -1,9 +1,9 @@
 class Ask < Formula
   desc "Ask lets you get shell commands using natural language by guessing your shell and prompting ChatGPT"
   homepage "https://github.com/damoonrashidi/homebrew_ask"
-  url "https://github.com/damoonrashidi/ask/archive/refs/tags/1.2.0.tar.gz"
-  version "1.2.0"
-  sha256 "45d5446397f78335e455aa79cf4ad489233df0d71e05b5117dd0f04a04e1ec84"
+  url "https://github.com/damoonrashidi/ask/archive/refs/tags/1.3.0.tar.gz"
+  version "1.3.0"
+  sha256 "feff2a4df4581d31a768be2891c0178ed24fe4702c6fe0ac2c573ec16bf13b63"
 
   depends_on "rust" => :build
 
